a3899d2add5dd96d85bfb35964d12595baf506d9
induja-1413034 <induja26.1997@gmail.com>
2016-12-22 09:12:27 +0530

    Add files via upload

---
+++ power.c
@@ -0,0 +1,13 @@
+#include<Stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("\nEnter the number:");
+scanf("%d",&a);
+printf("\nEnter the power:");
+scanf("%d",&b);
+c=pow(a,b);
+printf("%d",c);
+getch();
+}
