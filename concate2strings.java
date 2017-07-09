52aafc366e7e9406d803c70402175e490e2f9c84
induja-1413034 <induja26.1997@gmail.com>
2017-07-09 08:01:37 +0530

    Add files via upload

---
+++ concate2strings.java
@@ -0,0 +1,14 @@
+package guviprgms;
+
+import java.util.Scanner;
+
+public class concate2strings {
+public static void main(String[] args){
+	String a;
+	String b;
+	Scanner s=new Scanner(System.in);
+	a=s.next();
+	b=s.next();
+	System.out.println(a+b);
+}
+}
