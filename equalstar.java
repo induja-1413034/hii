6224646a5c7bd04b6c5a5cafadb2d1ad595dab00
induja-1413034 <induja26.1997@gmail.com>
2017-07-09 08:32:34 +0530

    Add files via upload

---
+++ equalstar.java
@@ -0,0 +1,17 @@
+package guviprgms;
+
+import java.util.Scanner;
+
+public class equalstar {
+public static void main(String[] args){
+	int a;
+	Scanner s=new Scanner(System.in);
+	a=s.nextInt();
+	for(int i=0;i<a;i++){
+		for(int j=0;j<a;j++){
+		System.out.print("*");
+	}
+		System.out.println("");
+}
+}
+}
